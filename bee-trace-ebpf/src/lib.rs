7f5bfe011d52927a00c65f93cb25ced18133e7c9
no-yan <63000297+no-yan@users.noreply.github.com>
2025-06-13 17:14:12 +0900

    refactor: use dedicated map variables

--- bee-trace-ebpf/src/lib.rs
+++ bee-trace-ebpf/src/lib.rs
@@ -1,3 +1,14 @@
 #![no_std]
 
 // This file exists to enable the library target.
+
+#[cfg(test)]
+extern crate std;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sanity() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- bee-trace-ebpf/src/main.rs
+++ bee-trace-ebpf/src/main.rs
@@ -1,11 +1,42 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{macros::socket_filter, programs::SkBuffContext};
+use aya_ebpf::{
+    macros::{map, socket_filter},
+    maps::HashMap,
+    programs::SkBuffContext,
+};
+use aya_log_ebpf::info;
+
+#[map(name = "BLOCK_LIST")]
+static mut BLOCK_LIST_MAP: HashMap<u32, u8> = HashMap::<u32, u8>::with_max_entries(1024, 0);
+
+#[map(name = "ALLOW_LIST")]
+static mut ALLOW_LIST_MAP: HashMap<u32, u8> = HashMap::<u32, u8>::with_max_entries(1024, 0);
 
 #[socket_filter]
-pub fn bee_trace(_ctx: SkBuffContext) -> i64 {
-    0
+pub fn bee_trace(ctx: SkBuffContext) -> i64 {
+    match unsafe { try_bee_trace(&ctx) } {
+        Ok(pass) => pass as i64,
+        Err(ret) => ret as i64,
+    }
+}
+
+unsafe fn try_bee_trace(ctx: &SkBuffContext) -> Result<u32, u32> {
+    let src = ctx.skb.remote_ipv4();
+    let dst = ctx.skb.local_ipv4();
+
+    if BLOCK_LIST_MAP.get(&src).is_some() || BLOCK_LIST_MAP.get(&dst).is_some() {
+        info!(ctx, "blocked {:i} -> {:i}", src, dst);
+        return Ok(0);
+    }
+
+    if ALLOW_LIST_MAP.get(&src).is_some() || ALLOW_LIST_MAP.get(&dst).is_some() {
+        info!(ctx, "allowed {:i} -> {:i}", src, dst);
+        return Ok(1);
+    }
+
+    Ok(1)
 }
 
 #[cfg(not(test))]
